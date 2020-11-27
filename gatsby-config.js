caa443a978dfe662eb5ea92385d43cb96872b2d1
AlannaCraft <alannaj121@gmail.com>
2020-11-27 17:45:33 +0000

    Added searchbar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
-require("dotenv").config({
+require('dotenv').config({
   path: `.env`,
 });
 
 module.exports = {
   plugins: [
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,16 +13,16 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-strapi",
+      resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
-        contentTypes: ["article", "category", "writer"],
+        apiURL: process.env.API_URL || 'http://localhost:1337',
+        contentTypes: ['article', 'category', 'writer'],
         singleTypes: [`homepage`, `global`],
         queryLimit: 10000,
       },
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -35,6 +35,63 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`,
       },
     },
-    "gatsby-plugin-offline",
+    {
+      resolve: 'gatsby-plugin-local-search',
+      options: {
+        // A unique name for the search index. This should be descriptive of
+        // what the index contains. This is required.
+        name: 'articles',
+
+        // Set the search engine to create the index. This is required.
+        // The following engines are supported: flexsearch, lunr
+        engine: 'flexsearch',
+
+        // GraphQL query used to fetch all data for the search index. This is
+        // required.
+        query: `
+          {
+            allStrapiArticle(
+              filter: {status: {eq: "published"}}
+            ) {
+              nodes {
+                title
+                description
+                content
+                slug
+                publishedAt
+              }
+            }
+          }
+        `,
+
+        // Field used as the reference value for each document.
+        // Default: 'id'.
+        ref: 'slug',
+
+        // List of keys to index. The values of the keys are taken from the
+        // normalizer function below.
+        // Default: all fields
+        index: ['title', 'description', 'content'],
+
+        // List of keys to store and make available in your UI. The values of
+        // the keys are taken from the normalizer function below.
+        // Default: all fields
+        store: ['title', 'slug', 'publishedAt'],
+
+        // Function used to map the result from the GraphQL query. This should
+        // return an array of items to index in the form of flat objects
+        // containing properties to index. The objects must contain the `ref`
+        // field above (default: 'id'). This is required.
+        normalizer: ({ data }) =>
+          data.allStrapiArticle.nodes.map(node => ({
+            title: node.title,
+            description: node.description,
+            content: node.content,
+            slug: node.slug,
+            publishedAt: node.publishedAt,
+          })),
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 };
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,100 @@
-import React from "react";
-import { Link, StaticQuery, graphql } from "gatsby";
+import React from 'react';
+import { useState } from 'react';
+import { Link, StaticQuery, graphql } from 'gatsby';
+import { useFlexSearch } from 'react-use-flexsearch';
+import Moment from 'react-moment';
+
+const Component = ({ data }) => {
+  const { index, store } = data.localSearchArticles;
+  const [query, setQuery] = useState('');
+
+  const searchArticles = useFlexSearch(query, index, store);
+  const onChange = event => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <nav className="uk-navbar-container" data-uk-navbar>
+          <div className="uk-navbar-left">
+            <ul className="uk-navbar-nav">
+              <li>
+                <Link to="/">{data.strapiGlobal.siteName}</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="uk-navbar-right">
+            <div>
+              <a
+                className="uk-navbar-toggle"
+                data-uk-search-icon
+                href=""
+                aria-label="Show Search"
+              ></a>
+              <div
+                className="uk-drop"
+                data-uk-drop="mode: click; pos: left-center; offset: 0"
+              >
+                <form className="uk-search uk-search-navbar uk-width-1-1 navbar-search">
+                  <input
+                    className="uk-search-input"
+                    type="search"
+                    placeholder="Search..."
+                    autoFocus={true}
+                    aria-label="Search"
+                    value={query}
+                    onChange={onChange}
+                  />
+                </form>
+                <ul className="uk-nav uk-dropdown-nav uk-nav-default searchbar-results">
+                  {searchArticles.map(article => (
+                    <li key={`article_${article.slug}`}>
+                      <Link to={`/article/${article.slug}`}>
+                        {`${article.title} - `}
+                        <span className="uk-text-meta">
+                          <Moment format="MMM Do YYYY">
+                            {article.published_at}
+                          </Moment>
+                        </span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <button
+              className="uk-button uk-button-default uk-margin-right"
+              type="button"
+            >
+              Categories
+            </button>
+            <div uk-dropdown="animation: uk-animation-slide-top-small; duration: 1000">
+              <ul className="uk-nav uk-dropdown-nav">
+                {data.allStrapiCategory.edges.map((category, i) => (
+                  <li key={`category__${category.node.slug}`}>
+                    <Link to={`/category/${category.node.slug}`}>
+                      {category.node.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 const Nav = () => (
   <StaticQuery
     query={graphql`
       query {
+        localSearchArticles {
+          index
+          store
+        }
         strapiGlobal {
           siteName
         }
@@ -18,40 +108,7 @@ const Nav = () => (
         }
       }
     `}
-    render={(data) => (
-      <div>
-        <div>
-          <nav className="uk-navbar-container" data-uk-navbar>
-            <div className="uk-navbar-left">
-              <ul className="uk-navbar-nav">
-                <li>
-                  <Link to="/">{data.strapiGlobal.siteName}</Link>
-                </li>
-              </ul>
-            </div>
-            <div className="uk-navbar-right">
-              <button
-                className="uk-button uk-button-default uk-margin-right"
-                type="button"
-              >
-                Categories
-              </button>
-              <div uk-dropdown="animation: uk-animation-slide-top-small; duration: 1000">
-                <ul className="uk-nav uk-dropdown-nav">
-                  {data.allStrapiCategory.edges.map((category, i) => (
-                    <li key={`category__${category.node.slug}`}>
-                      <Link to={`/category/${category.node.slug}`}>
-                        {category.node.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
-    )}
+    render={data => <Component data={data} />}
   />
 );
 
